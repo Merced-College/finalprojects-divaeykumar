94a35b3b9a4d048f97562df2777619bcf56cba9e
divaeykumar <154995894+divaeykumar@users.noreply.github.com>
2025-05-02 15:49:00 +0000

    Science Fair

--- src/CLI/Main.java
+++ src/CLI/Main.java
@@ -2,8 +2,32 @@ package CLI;
 
 public class Main {
     public static void main(String[] args) {
-        
+          Scanner sc = new Scanner(System.in); // Built-in java class used to read input
+          int choice;
+
+          while (true) {
+            System.out.println(" 1: Create a new task");
+            System.out.println("  2: Delete an existing task");
+            System.out.println(" 3: Show all current tasks");
+            System.out.println(" 0. Exit the Smart Task Manager");
+            System.out.print(" Select from option's 0–3: ");
+
+            // Based on what the user inputs depending on what the user is using the Smart task Manager for, one of the print statements will be displayed depending on not just what the user is utilizing the smart task manager for but also what he's inputing as well
+
+            if (choice == 1) {
+              System.out.println("Add selected Task"); 
+          } else if (choice == 2) {
+              System.out.println("Remove selected Task");
+              } else if (choice == 3) {
+              System.out.println("Show all selected Tasks");
+              } else if (choice == 0) {
+              System.out.println("Application closed: Thanks for using the Smart Task Manager");
+              return;
+              } else {
+              System.out.println("Error, try again"); // If the user inputs something that the program doesn't understand
+              }
+          
+          }
     }
 }
-
 
--- src/Model/Task.java
+++ src/Model/Task.java
@@ -1,5 +1,68 @@
 package Model;
 
 public class Task {
+    // Fields are implemented here, private variables are implemented here to store the task's data
+    private String name;
+    private String dueDate;
+    private int priority; 
 
+    // Constructors are implemented here, creates task with a certain name, due date and priority level.
+    /** 
+ * @param name      - Task name: 
+ * @param dueDate   - Due date: (YYYY-MM-DD)
+ * @param priority  - Importance: 1 (Very Important), 2 (Important), 3 (Not as Important but still important)
+ */
+
+ // @param purpose in this code is to describe a parameter of a method or constructor
+
+
+ //Here varibales are being defined for the task
+public Task(String name, String dueDate, int priority) {
+    this.name = name;
+    this.dueDate = dueDate;
+    this.priority = priority;
 }
+
+    // Getters are implemented here, the Getter Methods are implemented to be able to access current values of an object's properties
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    // Setters are implemented here, purpose of it whenever you want to add or chane the data stored with an object
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    // toString Method is implemented here, purpose of this method is to log the state of an object
+    @Override
+    public String toString() {
+        return "Task: " + name +
+            "\nDue: " + dueDate +
+            "\nPriority: " + priority;
+ 
+    }
+    // This part of the code incorporate the task name, due date, and priority into a block string
+}
+ 
+
+
+
+
+
